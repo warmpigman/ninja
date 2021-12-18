336821e37326787e351ced3b3f851e0d509a1e91
ProNinjaGamin0 <swithin.pais@gmail.com>
2021-12-18 15:40:25 +0000

    Added 1 argument support for dice.ts

--- Ninja/src/src/commands/Misc/Fun/dice.ts
+++ Ninja/src/src/commands/Misc/Fun/dice.ts
@@ -4,9 +4,11 @@ module.exports = {
     name: ["dice", "roll"],
     category: "Fun",
     description: "Roll a die between a floor and ceiling.",
-    usage: "dice [floor] [ceiling]",
+    usage: "dice [floor]\ndice [floor] [ceiling]",
     examples: [
-        "dice 1 6"
+        "dice",
+        "dice 420",
+        "dice 69 420"
     ],
     async execute(message: any, args: Array<string>, client: any, paguClient: any) {
         var embed = new paguClient.Discord.MessageEmbed()
@@ -17,7 +19,12 @@ module.exports = {
         if (args.length >= 2) {
             embed.setDescription(`**You rolled a ${Math.floor(Number(`.${require('crypto').randomBytes(4).toString('hex').split('').map((letter:String) => letter.charCodeAt(0)).join('')}`) * parseInt(args[1])) + parseInt(args[0])}!**`)
             message.channel.send({ embeds: [embed], allowedMentions: { "users": [] } });
-        } else {
+        }
+        else if (args.length == 1) {
+            embed.setDescription(`**You rolled a ${Math.floor(Math.random() * (parseInt(args[0]) + 1))}!**`)
+            message.channel.send({ embeds: [embed], allowedMentions: { "users": [] } });
+        }
+        else {
             embed.setDescription(`**You rolled a ${Math.floor(Number(`.${require('crypto').randomBytes(4).toString('hex').split('').map((letter:String) => letter.charCodeAt(0)).join('')}`) * 6) + 1}!**`)
             message.channel.send({ embeds: [embed], allowedMentions: { "users": [] } });
         }
