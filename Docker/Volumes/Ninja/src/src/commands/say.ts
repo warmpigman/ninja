36ca269b5dc9aa55754e67fb55bdb726cbac20b3
ProNinjaGamin0 <ProNinjaGamin0@users.noreply.github.com>
2022-01-01 16:45:45 +0000

    Formatting

--- Docker/Volumes/Ninja/src/src/commands/say.ts
+++ Docker/Volumes/Ninja/src/src/commands/say.ts
@@ -1,21 +1,20 @@
 module.exports = {
-    name: ["say"],
-    category: "Dev",
-    description: "It speaks",
-    usage: "say [speech]",
-    examples: ["say aaron is a noob"],
-    hidden: ["dev"],
-    async execute(
-      message: any,
-      args: Array<string>,
-      client: any,
-      paguClient: any
-    ) {
-        if (args[0].length == 0) {
-            message.channel.send("What should i say?")
-        }
-        else {
-            message.channel.send(args.join(" "))
-        }
+  name: ["say"],
+  category: "Dev",
+  description: "It speaks",
+  usage: "say [speech]",
+  examples: ["say aaron is a noob"],
+  hidden: ["dev"],
+  async execute(
+    message: any,
+    args: Array<string>,
+    client: any,
+    paguClient: any
+  ) {
+    if (args[0].length == 0) {
+      message.channel.send("What should i say?");
+    } else {
+      message.channel.send(args.join(" "));
     }
-}
+  },
+};
