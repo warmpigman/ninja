2df46202d8c55795c536dad667b0c98e80f13848
ProNinjaGamin0 <swithin.pais@gmail.com>
2022-02-19 20:41:23 +0000

    Added listener for silent vc

---
+++ Docker/Volumes/Ninja/src/src/events/voice-Join.ts
@@ -0,0 +1,17 @@
+import { VoiceState } from "discord.js";
+module.exports = {
+    event: "voiceStateUpdate",
+    async execute(client: any, paguClient: any, oldState:VoiceState, newState:VoiceState) {
+        newState.member.guild.roles.fetch().then(async (roles: any) => {
+            const silentVcRole = roles.find(
+                (r: any) => r.name.toLowerCase() === "silent vc"
+            )
+            if (newState.channel === null) {
+                await newState.member.roles.remove(silentVcRole)
+            }
+            else if (oldState.channel === null) {
+                await newState.member.roles.add(silentVcRole)
+            }
+        })
+    }
+}
