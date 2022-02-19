cb8a14a38c7eec2858548f545c06dcb5a0ca8fee
ProNinjaGamin0 <ProNinjaGamin0@users.noreply.github.com>
2022-02-19 20:42:06 +0000

    Formatting

--- Docker/Volumes/Ninja/src/src/events/voice-Join.ts
+++ Docker/Volumes/Ninja/src/src/events/voice-Join.ts
@@ -1,17 +1,21 @@
 import { VoiceState } from "discord.js";
 module.exports = {
-    event: "voiceStateUpdate",
-    async execute(client: any, paguClient: any, oldState:VoiceState, newState:VoiceState) {
-        newState.member.guild.roles.fetch().then(async (roles: any) => {
-            const silentVcRole = roles.find(
-                (r: any) => r.name.toLowerCase() === "silent vc"
-            )
-            if (newState.channel === null) {
-                await newState.member.roles.remove(silentVcRole)
-            }
-            else if (oldState.channel === null) {
-                await newState.member.roles.add(silentVcRole)
-            }
-        })
-    }
-}
+  event: "voiceStateUpdate",
+  async execute(
+    client: any,
+    paguClient: any,
+    oldState: VoiceState,
+    newState: VoiceState
+  ) {
+    newState.member.guild.roles.fetch().then(async (roles: any) => {
+      const silentVcRole = roles.find(
+        (r: any) => r.name.toLowerCase() === "silent vc"
+      );
+      if (newState.channel === null) {
+        await newState.member.roles.remove(silentVcRole);
+      } else if (oldState.channel === null) {
+        await newState.member.roles.add(silentVcRole);
+      }
+    });
+  },
+};
