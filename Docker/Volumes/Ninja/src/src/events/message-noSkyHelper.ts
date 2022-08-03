95f4b16c481ebc9e6fcc73d2dcb1a0de7f06d11f
warmpigman <warmpigman@users.noreply.github.com>
2022-08-03 17:42:37 +0000

    Formatting

--- Docker/Volumes/Ninja/src/src/events/message-noSkyHelper.ts
+++ Docker/Volumes/Ninja/src/src/events/message-noSkyHelper.ts
@@ -1,17 +1,18 @@
 import { Message, MessageEmbed } from "discord.js";
 module.exports = {
   event: "messageCreate",
-  async execute(
-    client: any,
-    paguClient: any,
-    message: Message
-  ) {
-    if(message.author.id == "710143953533403226" && message.channel.id !== "673251053252247553") {
-        await message.delete()
-        const msg = await message.channel.send("Do not use <@710143953533403226> here, go to <#673251053252247553>")
-        setTimeout(() => {
-            msg.delete()
-        }, 5000)
+  async execute(client: any, paguClient: any, message: Message) {
+    if (
+      message.author.id == "710143953533403226" &&
+      message.channel.id !== "673251053252247553"
+    ) {
+      await message.delete();
+      const msg = await message.channel.send(
+        "Do not use <@710143953533403226> here, go to <#673251053252247553>"
+      );
+      setTimeout(() => {
+        msg.delete();
+      }, 5000);
     }
   },
 };
