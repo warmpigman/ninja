2489e0f1060756748ea0f27c2b5399c7f2467af2
ProNinjaGamin0 <swithin.pais@gmail.com>
2022-10-22 20:19:19 +0100

    Added message-urlShortener.ts to delete links that redirect.

---
+++ Docker/Volumes/Ninja/src/src/events/message-urlShortener.ts
@@ -0,0 +1,26 @@
+import { Message, User } from "discord.js";
+import "axios";
+
+module.exports = {
+    event: "messageCreate",
+    async execute(client: any, paguClient: any, message: Message) {
+        const re = /(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+/;
+        let link = message.content?.match(re)?.[0];
+        if (link === undefined) {
+            return
+        }
+        link = link.includes("https://") || link.includes("http://") ? link : "http://" + link;
+        try {
+            axios.get({
+                url: link,
+                maxRedirects: 0
+                }
+            )
+        }
+        catch(e: any) {
+            if (e.response.status >= 300 && e.response.status < 400) {
+                await message.delete()
+            }
+        }
+    },
+}
