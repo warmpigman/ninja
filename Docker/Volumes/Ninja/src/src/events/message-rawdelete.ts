99d68708cdbea898464b1fe18405ab95dad1ceef
Warmpigman <72272017+warmpigman@users.noreply.github.com>
2022-10-23 19:31:59 +0000

    Seperate Docker compose configurations for dev and prod

--- Docker/Volumes/Ninja/src/src/events/message-rawdelete.ts
+++ Docker/Volumes/Ninja/src/src/events/message-rawdelete.ts
@@ -7,5 +7,37 @@ module.exports = {
     paguClient: any,
     message: Message,
   ) {
-    
+    const guildSchema = await paguClient.schemas.get("guild");
+    const guildData = await guildSchema.findOne({
+      guildID: message.guild?.id,
+    });
+    const mainLoggingChannel = guildData.mainLoggingChannel;
+    if(!mainLoggingChannel.Set) return;
+
+    const embed = new MessageEmbed();
+    embed
+      .setAuthor({  
+        name: `${message.author.tag}`,
+        iconURL: `${message.author.avatarURL({ dynamic: true })}`,
+      })
+      .setTitle(
+        `🗑️\n[Message](${message.url}) sent by <#${message.author.id}> deleted in <#${message.channelId}>`
+      )
+      .addFields([
+        { name: "Message", value: message.content ?? "None" },
+      ])
+      .setFooter(
+        `Message ID: ${message.id} • User ID: ${message.author.id}`
+      )
+      .setTimestamp(Date.now())
+      .setColor(`#1f94a6`);
+    // await oldMessage.channel.send({});
+    let attachments = [];
+    for (let attachment in message.attachments) {
+      attachments.push(attachment);
+    }
+    await mainLoggingChannel.send({
+      embeds: [embed],
+      attachments: attachments,
+    });
   }}
