a41c17afb6c0cf042444be596169636f225ef801
ProNinjaGamin0 <ProNinjaGamin0@users.noreply.github.com>
2022-10-30 22:06:14 +0000

    Formatting

--- Docker/Volumes/Ninja/src/src/commands/Moderation/purge.ts
+++ Docker/Volumes/Ninja/src/src/commands/Moderation/purge.ts
@@ -151,13 +151,16 @@ module.exports = {
                 embed.setDescription(
                   `:wastebasket: ${messagesToDelete.length} messages deleted in ${message.channel}`
                 );
-                return message.channel.send({
-                  embeds: [embed],
-                  allowedMentions: { users: [] },
-                }).then( msg => {
-                  setTimeout(() => {
-                    msg.delete()
-                  }, 5000)});
+                return message.channel
+                  .send({
+                    embeds: [embed],
+                    allowedMentions: { users: [] },
+                  })
+                  .then((msg) => {
+                    setTimeout(() => {
+                      msg.delete();
+                    }, 5000);
+                  });
               });
 
             // await hastebin.createPaste(findFileData(), { server: 'https://hastebin.com/' })
@@ -169,13 +172,16 @@ module.exports = {
             //         embed.setDescription(`:wastebasket: ${messagesToDelete.length} messages deleted in ${message.channel} (${url ?? "Hastebin could not be created"})`)
             //         return message.channel.send({ embeds: [embed], allowedMentions: { users: [] } })
             //     })
-            return message.channel.send({
-              embeds: [embed],
-              allowedMentions: { users: [] },
-            }).then( msg => {
-              setTimeout(() => {
-                msg.delete()
-              }, 5000)});;
+            return message.channel
+              .send({
+                embeds: [embed],
+                allowedMentions: { users: [] },
+              })
+              .then((msg) => {
+                setTimeout(() => {
+                  msg.delete();
+                }, 5000);
+              });
           }
         }
         var toFetch: any = findNextLimit();
@@ -317,25 +323,31 @@ module.exports = {
                 : "Hastebin could not be created"
             })`
           );
-          return message.channel.send({
-            embeds: [embed],
-            allowedMentions: { users: [] },
-          }).then( msg => {
-            setTimeout(() => {
-              msg.delete()
-            }, 5000)});;
+          return message.channel
+            .send({
+              embeds: [embed],
+              allowedMentions: { users: [] },
+            })
+            .then((msg) => {
+              setTimeout(() => {
+                msg.delete();
+              }, 5000);
+            });
         })
         .catch((err: Error) => {
           embed.setDescription(
             `:wastebasket: ${messagesToDelete.length} messages deleted in ${message.channel}`
           );
-          return message.channel.send({
-            embeds: [embed],
-            allowedMentions: { users: [] },
-          }).then( msg => {
-            setTimeout(() => {
-              msg.delete()
-            }, 5000)});;
+          return message.channel
+            .send({
+              embeds: [embed],
+              allowedMentions: { users: [] },
+            })
+            .then((msg) => {
+              setTimeout(() => {
+                msg.delete();
+              }, 5000);
+            });
         });
       // var File = await new MessageAttachment(Buffer.from(await findFileData(), 'utf8'), 'purge.txt');
       // await hastebin.createPaste(findFileData(), { server: 'https://hastebin.com/' })
@@ -351,13 +363,16 @@ module.exports = {
         await embed.setDescription(
           "I have deleted " + messagesToDelete.length + " messages!"
         );
-        const sentEmbed = await message.channel.send({
-          embeds: [embed],
-          allowedMentions: { users: [] },
-        }).then( msg => {
-          setTimeout(() => {
-            msg.delete()
-          }, 5000)});;
+        const sentEmbed = await message.channel
+          .send({
+            embeds: [embed],
+            allowedMentions: { users: [] },
+          })
+          .then((msg) => {
+            setTimeout(() => {
+              msg.delete();
+            }, 5000);
+          });
         setTimeout(() => {
           sentEmbed.delete();
         }, 10000);
